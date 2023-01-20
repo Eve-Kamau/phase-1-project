421ba7e24a1faa4506b8af255c68afce9cb63d11
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-01-21 02:07:32 +0300

    Dev Stage 4

--- dereva.js
+++ dereva.js
@@ -1,55 +1,30 @@
 const url_API = 'https://my-json-server.typicode.com/Eve-Kamau/phase-1-project/drivers';
 
-document.addEventListener('DOMContentLoaded',() => {
-    getOneDriver();
-})
+getDriverData();
 
-//Obtain One Driver Details
-function getOneDriver(id = 1){
-    fetch(`${url_API}/${id}`)
-    .then(response =>  response.json())
-    .then(driver  => {
-    console.log(driver);
+function getDriverData() {
+    fetch(url_API)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
 
-    getDriver(driver)
-    }) 
-    .catch((error) => {
-    console.log('ERROR!')
+    data.innerHTML = ''
+    data.forEach(driver => {
+        console.log(driver);
+    const li = document.createElement('li')
+
+    listItems.push(li)
+
+    li.innerHTML = `
+    <img src="${driver.driver_image}" alt="${driver.name}"
+    <div class ="driver-info">
+        <h4>"${driver.name}"</h4>
+        <p>${driver.driving_experience}, ${driver.description}, ${driver.city}</p>
+        `
     })
-}
-
-function getDriver(driver){
-    document.getElementById('id').textContent = drivers.id,
-    document.getElementById('name').textContent = drivers.name,
-    document.getElementById('experience').textContent = drivers.driving_experience
-    document.getElementById('description').textContent = drivers.description
-    document.getElementById('image').src = drivers.driver_image
-}   
-
-//FETCH ALL DRIVERS, DISPLAY MENU
-function getAllDrivers(){
-fetch(url_API)
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    const driverList = data.map(driver => drivers.name)
-    addDrivers(driverList)
 })
 .catch((error) =>  {
-console.log('Driver Not Available!')
-})
-getOneDriver();
-}
+    console.log('Driver Not Available!')
+    })
 
-function addDrivers(driverList){
-    //console.log(filmList)
-    films.innerHTML = null;
-    document.getElementById('films').replaceChildren()
-    const filmMenu = document.getElementById('films')
-    filmList.forEach(film => {
-        //console.log(film)
-        const li = document.createElement("li")
-        li.textContent = film
-        filmMenu.appendChild(li)
-    });
 }
